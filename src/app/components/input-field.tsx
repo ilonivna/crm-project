3e19e6f7aa4dcc7e91b6f623607656202cd23b20
ilonivna <147661751+ilonivna@users.noreply.github.com>
2024-10-19 00:23:29 +0300

    label added

--- src/app/components/input-field.tsx
+++ src/app/components/input-field.tsx
@@ -1,15 +1,15 @@
 'use client';
 
 import React from 'react';
-import {Field} from "formik";
+import { Field } from "formik";
 
 export interface InputFieldProps
-    extends React.HTMLAttributes<HTMLInputElement> {
+    extends React.InputHTMLAttributes<HTMLInputElement> {
     label?: string;
 }
 
 
-export default function InputField({ label, id, ...rest}: InputFieldProps) {
+export default function InputField({ label, id, ...rest }: InputFieldProps) {
     return (
         <div className='flex flex-col'>
             {label && (
--- src/app/components/logo-uploader.tsx
+++ src/app/components/logo-uploader.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import Image from 'next/image';
+import logo from "../../public/icons/upload.svg"
 
 export interface LogoUploaderProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'type'> {
     label?: string;
@@ -11,6 +12,22 @@ export default function LogoUploader({
     label, placeholder, id, ...rest
 }: LogoUploaderProps) {
     return (
-<div></div>
-    )
+<div className="flex gap-10 mb-3">
+    {label && <p className="text-base color-gray-900">{label}</p>}
+    <label htmlFor={id} className="flex flex-col items-center justify-center w-40 h-40 bg-white border border-slate-900 border-dashed rounded-full cursor-pointer">
+    <Image className="mb-1" src={logo} alt="upload"  width={48}
+           height={48}/>
+        {placeholder && (
+            <p className="text-base text-gray-500">{placeholder}</p>
+        )}
+        <input
+            {...rest}
+            id={id}
+            type="file"
+            accept="image/*"
+            className="hidden"
+        />
+    </label>
+</div>
+    );
 }
