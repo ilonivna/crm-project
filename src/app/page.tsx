bca7e5404354db41a78aab4e936346197f1ba17f
ilonivna <147661751+ilonivna@users.noreply.github.com>
2024-10-27 14:29:37 +0300

    proj completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
-
+'use client';
 import React from "react";
-import AddCompanyButton from "@/app/components/add-company-button";
+
+import Button from './components/button';
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+    const router = useRouter();
+
+
     return (
         <main>
-            <h1 className="text-3xl">Home page</h1>
-          <AddCompanyButton/>
+            <h1 className="text-3xl mb-6">Home page</h1>
+            <Button onClick={() => router.push('/dashboard', { scroll: false })}>
+                To dashboard
+            </Button>
         </main>
     );
 }
+
+
+
